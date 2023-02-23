75bb484901f1d534afc0b1e28b36081ca658ea4c
angit <angit@Chikkus_hp>
2023-02-23 20:06:31 +0530

    Element locator samples added

--- myselenium/src/main/java/com/obsqura/samples/Class2.java
+++ myselenium/src/main/java/com/obsqura/samples/Class2.java
@@ -37,15 +37,39 @@ public class Class2 {
 //		button.click();
 		
 		//Element locator by class
-		WebElement textbox=driver.findElement(By.className("class1"));
-		textbox.sendKeys("Angitha");
-		WebElement radio1=driver.findElement(By.className("class2"));
-		radio1.click();
-		WebElement radio2=driver.findElement(By.className("class3"));
-		radio2.click();
-		WebElement check=driver.findElement(By.className("class4"));
-		check.click();
-		WebElement button=driver.findElement(By.className("class5"));
-		button.click();
+//		WebElement textbox=driver.findElement(By.className("class1"));
+//		textbox.sendKeys("Angitha");
+//		WebElement radio1=driver.findElement(By.className("class2"));
+//		radio1.click();
+//		WebElement radio2=driver.findElement(By.className("class3"));
+//		radio2.click();
+//		WebElement check=driver.findElement(By.className("class4"));
+//		check.click();
+//		WebElement button=driver.findElement(By.className("class5"));
+//		button.click();
+		
+		//Element locator by linktext
+//		WebElement link=driver.findElement(By.linkText("Click me"));
+//		link.click();
+		
+		//Element locator by partial link text
+//		WebElement link=driver.findElement(By.partialLinkText("Click"));
+//		link.click();
+		
+		//Element locator by tag name
+//		WebElement tag=driver.findElement(By.tagName("a"));
+//		tag.click();
+		
+		//Element locator by xpath
+		WebElement xpath1=driver.findElement(By.xpath("//input[@id='001']"));
+		xpath1.sendKeys("Angitha");
+		WebElement xpath2=driver.findElement(By.xpath("//input[@class='class3']"));
+		xpath2.click();
+		WebElement xpath3=driver.findElement(By.xpath("//input[@name='D']"));
+		xpath3.click();
+		WebElement xpath4=driver.findElement(By.xpath("//input[@value='OK']"));
+		xpath4.click();
+		WebElement xpath5=driver.findElement(By.xpath("//a[@href=\"https://www.google.com\"]"));
+		xpath5.click();
 	}
 }
